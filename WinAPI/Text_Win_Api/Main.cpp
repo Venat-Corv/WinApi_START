7af86c0e2caf9bebde26faf229d057724d1439b0
Venator <skylinegvs@gmail.com>
2020-04-14 20:41:01 +0300

    WM_GET/SET_TEXT

--- WinAPI/Text_Win_Api/Main.cpp
+++ WinAPI/Text_Win_Api/Main.cpp
@@ -1,6 +1,12 @@
 #include<Windows.h>
 #include"resource.h"
 
+HWND hEdit1;
+HWND hEdit2;
+
+CHAR str1[] = "Hello gues ;-)";
+CHAR str2[] = { 0 };
+
 BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, int nCmdShow)
@@ -17,6 +23,13 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	{
 		HICON hIcon = LoadIcon(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_ICON1));
 		SendMessage(hwnd, WM_SETICON, 0, (LPARAM)hIcon);
+
+		hEdit1 = GetDlgItem(hwnd, IDC_EDIT1);
+		hEdit2 = GetDlgItem(hwnd, IDC_EDIT2);
+
+		SendMessage(hEdit1, WM_SETTEXT, 0, (LPARAM)str1);
+
+		SetFocus(GetDlgItem(hwnd, IDC_COPY));
 	}
 	break;
 	case WM_COMMAND:
@@ -27,7 +40,12 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 			break;
 		case IDCANCEL:
 			EndDialog(hwnd, 0);
+		case IDC_COPY:
+			SendMessage(hEdit1, WM_GETTEXT, 255, (LPARAM)str2);
+			SendMessage(hEdit2, WM_SETTEXT, 0, (LPARAM)str2);
+			break;
 		}
+		break;
 	case WM_CLOSE:
 		EndDialog(hwnd, 0);
 		break;
